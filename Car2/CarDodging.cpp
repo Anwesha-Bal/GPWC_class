b7f05c3433ecfbdc5a7f6c737d022817c853da23
Anwesha-Bal <smritiranjanbal@gmail.com>
2026-05-07 20:06:56 +0530

    Add files via upload

--- Car2/CarDodging.cpp
+++ Car2/CarDodging.cpp
@@ -7,21 +7,14 @@
 
 using namespace sf;
 
-enum class Lane
-{
-    LEFT,
-    RIGHT,
-    NONE
-};
-
 int main()
 {
     srand(static_cast<unsigned>(time(0)));
 
     enum class State
     {
-        PLAYING,
         PAUSED,
+        PLAYING,
         GAMEOVER
     };
 
@@ -42,88 +35,130 @@ int main()
 
     RenderWindow window(
         VideoMode(resolution.x, resolution.y),
-        "Car Dodging",
+        "Car Dodging Game",
         Style::Fullscreen
     );
 
-    View mainView(
-        FloatRect(
-            0,
-            0,
-            resolution.x,
-            resolution.y
-        )
-    );
+    window.setFramerateLimit(60);
 
     Font font;
 
     font.loadFromFile("./Assets/KOMIKAP_.ttf");
 
-    Text text;
+    Text messageText;
 
-    text.setFont(font);
-    text.setCharacterSize(60);
-    text.setFillColor(Color::Red);
+    messageText.setFont(font);
+    messageText.setCharacterSize(70);
+    messageText.setFillColor(Color::White);
 
-    FloatRect textRect = text.getLocalBounds();
-
-    text.setOrigin(
-        (textRect.left + textRect.width) / 2.0f,
-        (textRect.top + textRect.height) / 2.0f
+    messageText.setPosition(
+        resolution.x / 2 - 450,
+        resolution.y / 2 - 150
     );
 
-    text.setPosition(
-        resolution.x / 2.0f,
-        resolution.y / 2.0f
+    Text hud;
+
+    hud.setFont(font);
+
+    hud.setCharacterSize(45);
+
+    hud.setFillColor(Color::Yellow);
+
+    hud.setOutlineColor(Color::Black);
+
+    hud.setOutlineThickness(3);
+
+    hud.setPosition(50, 40);
+
+    float roadWidth = 700;
+
+    float leftBoundary =
+        resolution.x / 2 - roadWidth / 2;
+
+    float rightBoundary =
+        resolution.x / 2 + roadWidth / 2;
+
+    RectangleShape road;
+
+    road.setSize(
+        Vector2f(
+            roadWidth,
+            resolution.y
+        )
     );
 
-    text.setString("Press ENTER to Start");
+    road.setFillColor(
+        Color(40, 40, 40)
+    );
 
-    Clock clock;
+    road.setPosition(
+        leftBoundary,
+        0
+    );
 
-    bool paused = true;
+    const int numRoadLines = 20;
 
-    const int maxEnemies = 10;
+    RectangleShape roadLines[numRoadLines];
 
-    int carNum = 5;
+    float lineWidth = 15.0f;
+    float lineHeight = 120.0f;
+    float gap = 60.0f;
 
-    Lane lane[maxEnemies];
+    float centerX = resolution.x / 2.0f;
+
+    for (int i = 0; i < numRoadLines; i++)
+    {
+        roadLines[i].setSize(
+            Vector2f(
+                lineWidth,
+                lineHeight
+            )
+        );
+
+        roadLines[i].setFillColor(
+            Color::White
+        );
+
+        roadLines[i].setPosition(
+            centerX - lineWidth / 2.0f,
+            i * (lineHeight + gap)
+        );
+    }
+
+    float lanes[4];
+
+    lanes[0] = resolution.x / 2 - 250;
+    lanes[1] = resolution.x / 2 - 80;
+    lanes[2] = resolution.x / 2 + 80;
+    lanes[3] = resolution.x / 2 + 250;
+
+    PlayerCar player(
+        resolution.x / 2,
+        resolution.y - 220
+    );
+
+    const int maxEnemies = 20;
 
     EnemyCar* enemies[maxEnemies];
 
     for (int i = 0; i < maxEnemies; i++)
     {
         enemies[i] = nullptr;
-        lane[i] = Lane::NONE;
     }
+
+    int enemyLimit = 5;
+
+    float enemySpeedMultiplier = 0;
+
+    float spawnInterval = 1.5f;
 
     int score = 0;
 
-    PlayerCar player(
-        resolution.x / 2.0f,
-        resolution.y - 200.0f
-    );
+    float gameSpeed = 200;
 
-    if (level == Level::EASY)
-    {
-        carNum = 5;
-        player.setSpeed(100);
-    }
+    Clock clock;
 
-    if (level == Level::MEDIUM)
-    {
-        carNum = 7;
-        player.setSpeed(150);
-    }
-
-    if (level == Level::HARD)
-    {
-        carNum = 10;
-        player.setSpeed(200);
-    }
-
-    float spawnTimer = 0.0f;
-    float spawnInterval = 2.0f;
+    float spawnTimer = 0;
 
     while (window.isOpen())
     {
@@ -146,17 +181,58 @@ int main()
 
             if (
                 event.type == Event::KeyPressed &&
+                event.key.code == Keyboard::Num1
+            )
+            {
+                level = Level::EASY;
+
+                enemyLimit = 5;
+
+                spawnInterval = 1.5f;
+            }
+
+            if (
+                event.type == Event::KeyPressed &&
+                event.key.code == Keyboard::Num2
+            )
+            {
+                level = Level::MEDIUM;
+
+                enemyLimit = 8;
+
+                spawnInterval = 1.0f;
+            }
+
+            if (
+                event.type == Event::KeyPressed &&
+                event.key.code == Keyboard::Num3
+            )
+            {
+                level = Level::HARD;
+
+                enemyLimit = 12;
+
+                spawnInterval = 0.7f;
+            }
+
+            if (
+                event.type == Event::KeyPressed &&
                 event.key.code == Keyboard::Enter
             )
             {
-                paused = false;
                 state = State::PLAYING;
 
                 score = 0;
 
+                gameSpeed = 200;
+
+                enemySpeedMultiplier = 0;
+
+                spawnTimer = 0;
+
                 player.setPosition(
-                    resolution.x / 2.0f,
-                    resolution.y - 200.0f
+                    resolution.x / 2,
+                    resolution.y - 220
                 );
 
                 for (int i = 0; i < maxEnemies; i++)
@@ -164,18 +240,34 @@ int main()
                     if (enemies[i] != nullptr)
                     {
                         delete enemies[i];
+
                         enemies[i] = nullptr;
                     }
                 }
+            }
 
-                text.setString("");
+            if (
+                event.type == Event::KeyPressed &&
+                event.key.code == Keyboard::P
+            )
+            {
+                if (state == State::PLAYING)
+                {
+                    state = State::PAUSED;
+                }
+                else if (state == State::PAUSED)
+                {
+                    state = State::PLAYING;
+                }
             }
         }
 
         Time dt = clock.restart();
 
-        if (!paused)
+        if (state == State::PLAYING)
         {
+            gameSpeed += 5 * dt.asSeconds();
+
             if (Keyboard::isKeyPressed(Keyboard::A))
             {
                 player.moveLeft();
@@ -196,30 +288,41 @@ int main()
 
             player.update(dt);
 
-            if (spawnInterval > 0.6f)
+            for (int i = 0; i < numRoadLines; i++)
             {
-                spawnInterval -= 0.05f * dt.asSeconds();
+                roadLines[i].move(
+                    0,
+                    gameSpeed * dt.asSeconds()
+                );
+
+                if (
+                    roadLines[i].getPosition().y >
+                    resolution.y
+                )
+                {
+                    roadLines[i].setPosition(
+                        centerX - lineWidth / 2.0f,
+                        -lineHeight
+                    );
+                }
             }
 
             spawnTimer += dt.asSeconds();
 
             if (spawnTimer >= spawnInterval)
             {
-                for (int i = 0; i < carNum; i++)
+                for (int i = 0; i < enemyLimit; i++)
                 {
                     if (enemies[i] == nullptr)
                     {
                         enemies[i] = new EnemyCar();
 
-                        float randomX =
-                            (rand() %
-                            (int)(resolution.x - 200))
-                            + 100;
+                        int randomLane = rand() % 4;
 
                         enemies[i]->spawn(
-                            randomX,
+                            lanes[randomLane],
                             rand() % 5,
-                            score * 10.0f
+                            enemySpeedMultiplier
                         );
 
                         break;
@@ -229,10 +332,15 @@ int main()
                 spawnTimer = 0;
             }
 
-            for (int i = 0; i < carNum; i++)
+            for (int i = 0; i < enemyLimit; i++)
             {
                 if (enemies[i] != nullptr)
                 {
+                    enemies[i]->setSpeed(
+                        enemies[i]->getSpeed() +
+                        gameSpeed * 0.01f
+                    );
+
                     enemies[i]->update(dt);
 
                     if (
@@ -242,10 +350,6 @@ int main()
                     )
                     {
                         state = State::GAMEOVER;
-
-                        paused = true;
-
-                        text.setString("GAME OVER");
                     }
 
                     if (enemies[i]->isOffScreen())
@@ -255,23 +359,57 @@ int main()
                         enemies[i] = nullptr;
 
                         score++;
+
+                        enemySpeedMultiplier += 1.5f;
                     }
                 }
             }
         }
 
-        mainView.setCenter(
-            resolution.x / 2.0f,
-            resolution.y / 2.0f
+        std::stringstream ss;
+
+        ss << "SCORE : " << score<< "SPEED : "<< static_cast<int>(gameSpeed);
+
+        hud.setString(ss.str());
+
+        if (state == State::PAUSED)
+        {
+            messageText.setString(
+                "PRESS ENTER TO START\n\n"
+                "1 = EASY\n"
+                "2 = MEDIUM\n"
+                "3 = HARD\n\n"
+                "A/D = MOVE\n"
+                "P = PAUSE"
+            );
+        }
+
+        if (state == State::GAMEOVER)
+        {
+            messageText.setString(
+                "GAME OVER\n\n"
+                "PRESS ENTER TO RESTART"
+            );
+        }
+
+        window.clear(
+            Color(20, 120, 20)
         );
 
-        window.clear();
+        window.draw(road);
 
-        window.setView(mainView);
+        for (int i = 0; i < numRoadLines; i++)
+        {
+            window.draw(
+                roadLines[i]
+            );
+        }
 
-        window.draw(player.getSprite());
+        window.draw(
+            player.getSprite()
+        );
 
-        for (int i = 0; i < carNum; i++)
+        for (int i = 0; i < enemyLimit; i++)
         {
             if (enemies[i] != nullptr)
             {
@@ -281,9 +419,14 @@ int main()
             }
         }
 
-        if (paused)
+        window.draw(hud);
+
+        if (
+            state == State::PAUSED ||
+            state == State::GAMEOVER
+        )
         {
-            window.draw(text);
+            window.draw(messageText);
         }
 
         window.display();
